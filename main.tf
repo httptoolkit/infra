51a7ee55cf5d954510a69cd676d65f862e0f8ddd
Tim Perry <pimterry@gmail.com>
2025-12-05 11:16:29 +0100

    Add missing version, region & zone specifiers

--- main.tf
+++ main.tf
@@ -1,7 +1,9 @@
 terraform {
+  required_version = ">= 1.10.7"
   required_providers {
     scaleway = {
       source = "scaleway/scaleway"
+      version = "^2.64.0"
     }
   }
 }
@@ -10,19 +12,18 @@ provider "scaleway" {
   project_id = var.project_id
 }
 
-# 1. Private Network (VPC)
 resource "scaleway_vpc_private_network" "main" {
   name = "k8s-db-private-network"
   tags = ["tofu-playground"]
 }
 
-# 2. Managed Database (PostgreSQL)
 resource "scaleway_rdb_instance" "main" {
   name           = "playground-db"
   node_type      = "db-dev-s"
   engine         = "PostgreSQL-15"
   is_ha_cluster  = false
   disable_backup = true
+  region         = var.region
   user_name      = "tofu_user"
   password       = var.db_password
   tags           = ["tofu-playground"]
@@ -33,7 +34,6 @@ resource "scaleway_rdb_instance" "main" {
   }
 }
 
-# 3. Kapsule Cluster (Control Plane)
 resource "scaleway_k8s_cluster" "main" {
   name                        = "playground-cluster"
   version                     = "1.34"
@@ -49,11 +49,12 @@ resource "scaleway_k8s_cluster" "main" {
   }
 }
 
-# 4. Kapsule Node Pool
 resource "scaleway_k8s_pool" "main" {
   cluster_id  = scaleway_k8s_cluster.main.id
   name        = "default-pool"
   node_type   = "PLAY2-NANO"
+  region      = var.region
+  zone        = var.zone
   size        = 1
   min_size    = 1
   max_size    = 1

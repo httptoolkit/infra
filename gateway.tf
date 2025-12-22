c64ce541f79c545325d4a132f5e1d897c90965a9
Tim Perry <pimterry@gmail.com>
2025-12-22 17:49:51 +0000

    Properly persistently manage gateway IPs here

--- gateway.tf
+++ gateway.tf
@@ -49,10 +49,34 @@ resource "kubectl_manifest" "gateway_class" {
   depends_on = [helm_release.envoy_gateway]
 }
 
+resource "scaleway_lb_ip" "primary_ingress_ipv4" {
+  zone = var.primary_zone
+}
+
+resource "scaleway_lb_ip" "primary_ingress_ipv6" {
+  zone    = var.primary_zone
+  is_ipv6 = true
+}
+
+resource "scaleway_lb_ip" "secondary_ingress_ipv4" {
+  zone = var.secondary_zone
+}
+
+resource "scaleway_lb_ip" "secondary_ingress_ipv6" {
+  zone    = var.secondary_zone
+  is_ipv6 = true
+}
+
 locals {
-  gateway_topology = {
-    "primary-gateway"   = var.primary_zone
-    "secondary-gateway" = var.secondary_zone
+  gateway_zone_map = {
+    "primary-gateway" = {
+      zone = var.primary_zone,
+      ips  = [scaleway_lb_ip.primary_ingress_ipv4, scaleway_lb_ip.primary_ingress_ipv6]
+    },
+    "secondary-gateway" = {
+      zone = var.secondary_zone,
+      ips  = [scaleway_lb_ip.secondary_ingress_ipv4, scaleway_lb_ip.secondary_ingress_ipv6]
+    }
   }
 
   gateway_listeners = [
@@ -105,7 +129,7 @@ locals {
 }
 
 resource "kubectl_manifest" "gateways" {
-  for_each = local.gateway_topology
+  for_each = local.gateway_zone_map
 
   yaml_body = yamlencode({
     apiVersion = "gateway.networking.k8s.io/v1"
@@ -119,7 +143,8 @@ resource "kubectl_manifest" "gateways" {
       listeners        = local.gateway_listeners
       infrastructure = {
         annotations = {
-          "service.beta.kubernetes.io/scw-loadbalancer-zone"              = each.value
+          "service.beta.kubernetes.io/scw-loadbalancer-zone"              = each.value.zone
+          "service.beta.kubernetes.io/scw-loadbalancer-ip-ids"            = join(",", [for ip in each.value.ips : split("/", ip.id)[1]])
           "service.beta.kubernetes.io/scw-loadbalancer-type"              = "LB-S"
           "service.beta.kubernetes.io/scw-loadbalancer-use-hostname"      = "true"
           "service.beta.kubernetes.io/scw-loadbalancer-proxy-protocol-v2" = "true"
@@ -135,7 +160,7 @@ resource "kubectl_manifest" "gateways" {
 
 # Force HTTP/2 for all endpoint admin TLS connections:
 resource "kubectl_manifest" "force_h2_endpoint_admin_policy" {
-  for_each = local.gateway_topology
+  for_each = local.gateway_zone_map
 
   yaml_body = yamlencode({
     apiVersion = "gateway.envoyproxy.io/v1alpha1"
@@ -161,7 +186,7 @@ resource "kubectl_manifest" "force_h2_endpoint_admin_policy" {
 
 # Force HTTP/2 for all endpoint admin TLS connections:
 resource "kubectl_manifest" "allow_h2_public_endpoint_policy" {
-  for_each = local.gateway_topology
+  for_each = local.gateway_zone_map
 
   yaml_body = yamlencode({
     apiVersion = "gateway.envoyproxy.io/v1alpha1"
@@ -187,7 +212,7 @@ resource "kubectl_manifest" "allow_h2_public_endpoint_policy" {
 
 # Enable proxy protocol parsing, to handle proxy info from Scaleway LB:
 resource "kubectl_manifest" "proxy_protocol_policy" {
-  for_each = local.gateway_topology
+  for_each = local.gateway_zone_map
 
   yaml_body = yamlencode({
     apiVersion = "gateway.envoyproxy.io/v1alpha1"
@@ -207,4 +232,14 @@ resource "kubectl_manifest" "proxy_protocol_policy" {
   })
 
   depends_on = [kubectl_manifest.gateways]
+}
+
+output "gateway_ips" {
+  value = {
+    for gw_name, gw in local.gateway_zone_map :
+    gw_name => {
+      zone = gw.zone,
+      ips  = [for ip in gw.ips : ip.ip_address]
+    }
+  }
 }
